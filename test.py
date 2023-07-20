070c4d72b8082fba7675ffdde58e6de520bb0458
AmaliyaG <labirintsnov007@gmail.com>
2023-07-20 17:57:32 +0300

    Alex

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from src.helper.namespace import namespace as ns
 from src.helper.help_func import open_xml
 
 
-template_xml = open_xml('templates/contract.xml')
-outgoing_xml = open_xml('outgoing/14433491_xml.xml')
-
-template = template_xml.findall('.//ns4:contract/ns2:regNum', ns)
-outgoing = outgoing_xml.findall('.//ns1:data/ns2:regNum', ns)
+# template_xml = open_xml('templates/contract.xml')
+# outgoing_xml = open_xml('outgoing/14433491_xml.xml')
+#
+# template = template_xml.findall('.//ns4:contract/ns2:regNum', ns)
+# outgoing = outgoing_xml.findall('.//ns1:data/ns2:regNum', ns)
 
 print(len(template))
 print(len(outgoing))
@@ -16,3 +16,4 @@ print()
 [print(i) for i in outgoing]
 
 print('ПРивте Амалия')
+print('Alex1')
