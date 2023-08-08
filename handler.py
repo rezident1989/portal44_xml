1949b1a4d8eea8024e06324275dbd7c3123c9293
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2023-08-08 10:00:09 +0300

    updated handler.py

--- handler.py
+++ handler.py
@@ -5,22 +5,17 @@ from src.actions.tender_plan import tender_plan_2020
 import src.actions.notification as notification
 from src.actions.contract import contract
 
-
 if __name__ == '__main__':
     path_xml = 'outgoing/14433512_xml 07.08.xml'
     validate_xsd(path_xml)
-    file_xml = open_xml(path_xml)
+    root_tag = open_xml(path_xml).getroot().tag
 
-    if 'epNotificationEF2020' in file_xml.getroot().tag:
-        confirmation(path_xml)
+    confirmation(path_xml)
+    if 'tenderPlan2020' in root_tag:
+        tender_plan_2020(path_xml)
+    elif 'epNotificationEF2020' in root_tag:
         notification.ep_notification_ef_2020(path_xml)
         notification.ep_protocol_ef_2020_submit_offers(path_xml)
         notification.ep_protocol_ef_2020_final(path_xml)
-    elif 'tenderPlan2020' in file_xml.getroot().tag:
-        confirmation(path_xml)
-        tender_plan_2020(path_xml)
-    elif 'contract' in file_xml.getroot().tag:
-        confirmation(path_xml)
+    elif 'contract' in root_tag:
         contract(path_xml)
-    else:
-        confirmation(path_xml)
