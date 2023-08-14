9fff80c3927c62e39f24672cccbba342b95f524e
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2023-08-14 18:04:52 +0300

    update test

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from src.helper.namespace import namespace as ns
 from src.helper.help_func import open_xml
 
-xml_1 = open_xml('incoming/16.34_14.08.23_tenderPlan2020.xml').getroot()
-tag_1 = './/ns3:specialPurchasePosition/ns3:publishYear'
-tag_list_1 = xml_1.findall(xml_1, ns)
+xml_1 = open_xml('incoming/17.53_14.08.23_tenderPlan2020.xml').getroot()
+tag_list_1 = xml_1.findall('.//ns3:specialPurchasePosition/ns3:publishYear', ns)
 print('ДОКУМЕНТ_1')
 print(f'Количество тегов: {len(tag_list_1)}')
 [print(i.text) for i in tag_list_1]
 
-xml_2 = open_xml('incoming/16.34_14.08.23_tenderPlan2020.xml').getroot()
-tag_2 = './/ns3:specialPurchasePosition/ns3:publishYear'
-tag_list_2 = xml_2.findall(xml_1, ns)
+print('-' * 40)
+
+xml_2 = open_xml('templates/tenderPlan2020.xml').getroot()
+tag_list_2 = xml_2.findall('.//ns3:specialPurchasePosition/ns3:publishYear', ns)
 print('ДОКУМЕНТ_2')
 print(f'Количество тегов: {len(tag_list_2)}')
 [print(i.text) for i in tag_list_2]
