48caec851dc5de4d60b5146494548181ab34bcfc
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2023-08-17 18:32:31 +0300

    update2 contract_procedure.py

--- src/actions/contract_procedure.py
+++ src/actions/contract_procedure.py
@@ -1,6 +1,8 @@
+import copy
 from datetime import datetime
 from src.helper.namespace import namespace as ns
 from src.helper.help_func import open_xml, random_number
+import xml.etree.ElementTree as ET
 
 
 def contract_procedure(outgoing_xml):
@@ -8,6 +10,39 @@ def contract_procedure(outgoing_xml):
 
     tree = open_xml(outgoing_xml)
     template = open_xml("templates/contractProcedure.xml")
+
+    count_pay_doc = len(tree.findall('.//ns2:execution/ns2:payDoc', ns))
+    count_doc_acceptance = len(tree.findall('.//ns2:execution/ns2:docAcceptance', ns))
+    count_doc_execution = len(tree.findall('.//ns2:execution/ns2:docExecution', ns))
+
+    if count_pay_doc == 0:
+        template.find('.//ns2:executions', ns).remove(template.find(
+            './/ns2:execution/ns2:payDoc/..', ns))
+    elif count_pay_doc > 1:
+        member = copy.deepcopy(template.find('.//ns2:execution/ns2:payDoc/..', ns))
+        for i in range(2, count_pay_doc + 1):
+            template.find(".//ns2:executions", ns).append(member)
+
+    if count_doc_acceptance == 0:
+        template.find('.//ns2:executions', ns).remove(template.find(
+            './/ns2:execution/ns2:docAcceptance/..', ns))
+    elif count_doc_acceptance > 1:
+        member = copy.deepcopy(template.find('.//ns2:execution/ns2:docAcceptance/..', ns))
+        for i in range(2, count_doc_acceptance + 1):
+            template.find(".//ns2:executions", ns).append(member)
+
+    if count_doc_execution == 0:
+        template.find('.//ns2:executions', ns).remove(template.find(
+            './/ns2:execution/ns2:docExecution/..', ns))
+    elif count_doc_execution > 1:
+        member = copy.deepcopy(template.find('.//ns2:execution/ns2:docExecution/..', ns))
+        for i in range(2, count_doc_execution + 1):
+            template.find(".//ns2:executions", ns).append(member)
+
+    # TODO
+    template = ET.ElementTree(template)
+    template.write('venv/temp.xml', encoding='utf-8', xml_declaration=True)
+    template = open_xml('venv/temp.xml')
 
     template.find('.//ns2:id', ns).text = random_number(8)
     template.find('.//ns2:regNum', ns).text = tree.find('.//ns2:regNum', ns).text
@@ -23,54 +58,57 @@ def contract_procedure(outgoing_xml):
     template.find('.//ns2:stage/ns2:externalSid', ns).text = tree.find(
         './/ns2:stage/ns2:externalSid', ns).text
 
-    try:
-        template.find('.//ns2:execution/ns2:payDoc/ns2:sid', ns).text = tree.find(
-            './/ns2:execution/ns2:payDoc/ns2:sid', ns).text
-    except AttributeError:
-        template.find('.//ns2:execution/ns2:payDoc/ns2:sid', ns).text = random_number(8)
+    for i in range(1, count_pay_doc + 1):
+        try:
+            template.find(f'.//ns2:execution[{i}]/ns2:payDoc/ns2:sid', ns).text = tree.find(
+                f'.//ns2:execution[{i}]/ns2:payDoc/ns2:sid', ns).text
+        except AttributeError:
+            template.find(f'.//ns2:execution[{i}]/ns2:payDoc/ns2:sid', ns).text = random_number(8)
 
-    template.find('.//ns2:execution/ns2:payDoc/ns2:externalSid', ns).text = tree.find(
-        './/ns2:execution/ns2:payDoc/ns2:externalSid', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:payDoc/ns2:externalSid', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:payDoc/ns2:externalSid', ns).text
 
-    template.find('.//ns2:execution/ns2:payDoc/ns2:documentName', ns).text = tree.find(
-        './/ns2:execution/ns2:payDoc/ns2:documentName', ns).text
-    template.find('.//ns2:payDoc/ns2:documentDate', ns).text = tree.find(
-        './/ns2:execution/ns2:payDoc/ns2:documentDate', ns).text
-    template.find('.//ns2:payDoc/ns2:documentNum', ns).text = tree.find(
-        './/ns2:execution/ns2:payDoc/ns2:documentNum', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:payDoc/ns2:documentName', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:payDoc/ns2:documentName', ns).text
+        template.find('.//ns2:payDoc/ns2:documentDate', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:payDoc/ns2:documentDate', ns).text
+        template.find('.//ns2:payDoc/ns2:documentNum', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:payDoc/ns2:documentNum', ns).text
 
-    try:
-        template.find('.//ns2:execution/ns2:docAcceptance/ns2:sid', ns).text = tree.find(
-            './/ns2:execution/ns2:docAcceptance/ns2:sid', ns).text
-    except AttributeError:
-        template.find('.//ns2:execution/ns2:docAcceptance/ns2:sid', ns).text = random_number(8)
+    for i in range(1, count_doc_acceptance + 1):
+        try:
+            template.find(f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:sid', ns).text = tree.find(
+                f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:sid', ns).text
+        except AttributeError:
+            template.find(f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:sid', ns).text = random_number(8)
 
-    template.find('.//ns2:execution/ns2:docAcceptance/ns2:externalSid', ns).text = tree.find(
-        './/ns2:execution/ns2:docAcceptance/ns2:externalSid', ns).text
-    template.find('.//ns2:execution/ns2:docAcceptance/ns2:code', ns).text = tree.find(
-        './/ns2:execution/ns2:docAcceptance/ns2:code', ns).text
-    template.find('.//ns2:execution/ns2:docAcceptance/ns2:name', ns).text = tree.find(
-        './/ns2:execution/ns2:docAcceptance/ns2:name', ns).text
-    template.find('.//ns2:execution/ns2:docAcceptance/ns2:documentDate', ns).text = tree.find(
-        './/ns2:execution/ns2:docAcceptance/ns2:documentDate', ns).text
-    template.find('.//ns2:execution/ns2:docAcceptance/ns2:deliveryAcceptDate', ns).text = tree.find(
-        './/ns2:execution/ns2:docAcceptance/ns2:deliveryAcceptDate', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:externalSid', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:externalSid', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:code', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:code', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:name', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:name', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:documentDate', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:documentDate', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:deliveryAcceptDate', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docAcceptance/ns2:deliveryAcceptDate', ns).text
 
-    try:
-        template.find('.//ns2:execution/ns2:docExecution/ns2:sid', ns).text = tree.find(
-            './/ns2:execution/ns2:docExecution/ns2:sid', ns).text
-    except AttributeError:
-        template.find('.//ns2:execution/ns2:docExecution/ns2:sid', ns).text = random_number(8)
+    for i in range(1, count_doc_execution + 1):
+        try:
+            template.find(f'.//ns2:execution[{i}]/ns2:docExecution/ns2:sid', ns).text = tree.find(
+                f'.//ns2:execution[{i}]/ns2:docExecution/ns2:sid', ns).text
+        except AttributeError:
+            template.find(f'.//ns2:execution[{i}]/ns2:docExecution/ns2:sid', ns).text = random_number(8)
 
-    template.find('.//ns2:execution/ns2:docExecution/ns2:externalSid', ns).text = tree.find(
-        './/ns2:execution/ns2:docExecution/ns2:externalSid', ns).text
-    template.find('.//ns2:execution/ns2:docExecution/ns2:code', ns).text = tree.find(
-        './/ns2:execution/ns2:docExecution/ns2:code', ns).text
-    template.find('.//ns2:execution/ns2:docExecution/ns2:name', ns).text = tree.find(
-        './/ns2:execution/ns2:docExecution/ns2:name', ns).text
-    template.find('.//ns2:execution/ns2:docExecution/ns2:documentDate', ns).text = tree.find(
-        './/ns2:execution/ns2:docExecution/ns2:documentDate', ns).text
-    template.find('.//ns2:execution/ns2:docExecution/ns2:documentNum', ns).text = tree.find(
-        './/ns2:execution/ns2:docExecution/ns2:documentNum', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docExecution/ns2:externalSid', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docExecution/ns2:externalSid', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docExecution/ns2:code', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docExecution/ns2:code', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docExecution/ns2:name', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docExecution/ns2:name', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docExecution/ns2:documentDate', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docExecution/ns2:documentDate', ns).text
+        template.find(f'.//ns2:execution[{i}]/ns2:docExecution/ns2:documentNum', ns).text = tree.find(
+            f'.//ns2:execution[{i}]/ns2:docExecution/ns2:documentNum', ns).text
 
     return template
--- test.py
+++ test.py
@@ -2,16 +2,16 @@ from src.helper.namespace import namespace as ns
 from src.helper.help_func import open_xml
 
 
-xml_1 = open_xml('incoming/14.42_17.08.23_contractProcedure.xml')
-tag_list_1 = xml_1.findall('.//ns2:execution/ns2:payDoc/ns2:externalSid', ns)
-print('ДОКУМЕНТ №1')
-print(f'Количество тегов: {len(tag_list_1)}')
-[print(i.text) for i in tag_list_1]
+# xml_1 = open_xml('incoming/14.42_17.08.23_contractProcedure.xml')
+# tag_list_1 = xml_1.findall('.//ns2:execution/ns2:payDoc', ns)
+# print('ДОКУМЕНТ №1')
+# print(f'Количество тегов: {len(tag_list_1)}')
+# [print(i.text) for i in tag_list_1]
 
 print('-' * 40)
 
-xml_2 = open_xml('outgoing/14922509_xml.xml')
-tag_list_2 = tag_list_1
+xml_2 = open_xml('outgoing/14922514_xml.xml')
+tag_list_2 = xml_2.findall('.//ns2:execution/ns2:docAcceptance', ns)
 print('ДОКУМЕНТ №2')
 print(f'Количество тегов: {len(tag_list_2)}')
 [print(i.text) for i in tag_list_2]
