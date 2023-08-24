6cb67b9f61704c8d25801c856459c2b22ce2819e
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2023-08-24 15:08:49 +0300

    update11

--- scrips/search_xml.py
+++ scrips/search_xml.py
@@ -9,7 +9,7 @@ def search_xml(text):
     for i in os.listdir('../ftp_server'):
         if count_res != 0:
             break
-        with zipfile.ZipFile(f"ftp_server/{i}", "r") as myzip:
+        with zipfile.ZipFile(f"../ftp_server/{i}", "r") as myzip:
             for file_info in myzip.infolist():
                 if file_info.filename.endswith('.xml'):
                     if byte_object in myzip.read(file_info):
