7eff90c54df9ccea2d6dead2426b2584c8d90f5b
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2023-08-24 15:18:19 +0300

    update12

--- scrips/download_from_ftp.py
+++ scrips/download_from_ftp.py
@@ -9,8 +9,12 @@ ftp = ftplib.FTP(ftp_host, ftp_user, ftp_password)
 ftp.cwd('fcs_regions/Sankt-Peterburg/contracts/currMonth')
 server_filenames = ftp.nlst()
 
+if not os.path.exists(os.path.join('..', 'ftp_server')):
+    os.makedirs(os.path.join('..', 'ftp_server'))
+
+
 for server_filename in server_filenames:
-    full_name_file = os.path.join('../src', '..', 'ftp_server', server_filename)
+    full_name_file = os.path.join('..', 'ftp_server', server_filename)
     if server_filename not in os.listdir(os.path.abspath(os.path.join('../src', '..', 'ftp_server'))):
         with open(full_name_file, 'wb') as f:
             ftp.retrbinary('RETR ' + server_filename, f.write)
