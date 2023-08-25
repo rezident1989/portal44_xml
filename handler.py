51c87469ff35e0451027114d5575e8082559bce2
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2023-08-25 13:57:43 +0300

    25.08.2023 - 3

--- handler.py
+++ handler.py
@@ -14,41 +14,43 @@ def main(path, validation=True, send=True):
     clear_folder('incoming')
     files_to_send = []
     files_for_validation = []
-    validate_xsd(path)
-    server_address = get_server_address(path)
-    type_xml = get_type_xml(path)
-    print(path, 'this is', type_xml)
+    try:
+        validate_xsd(path)
+        server_address = get_server_address(path)
+        type_xml = get_type_xml(path)
+        print(path, 'this is', type_xml)
 
-    files_to_send.extend([create_xml(confirmation(path))])
+        files_to_send.extend([create_xml(confirmation(path))])
 
-    if ('tenderPlan2020' == type_xml) or ('tenderPlan2020Change' == type_xml):
-        files = [create_xml(tender_plan_2020(path))]
-    elif 'epNotificationEF2020' == type_xml:
-        functions = [ep_notification_ef_2020, ep_protocol_ef_2020_submit_offers, ep_protocol_ef_2020_final]
-        files = [create_xml(func(path)) for func in functions]
-    elif 'contract' == type_xml or 'closedContract' == type_xml:
-        files = [create_xml(contract(path))]
-    elif 'contractProcedure' == type_xml:
-        files = [create_xml(contract_procedure(path))]
-    else:
-        print('Нет обработки для пакета:', type_xml)
-        sys.exit(1)
+        if ('tenderPlan2020' == type_xml) or ('tenderPlan2020Change' == type_xml):
+            files = [create_xml(tender_plan_2020(path))]
+        elif 'epNotificationEF2020' == type_xml:
+            functions = [ep_notification_ef_2020, ep_protocol_ef_2020_submit_offers, ep_protocol_ef_2020_final]
+            files = [create_xml(func(path)) for func in functions]
+        elif 'contract' == type_xml or 'closedContract' == type_xml:
+            files = [create_xml(contract(path))]
+        elif 'contractProcedure' == type_xml:
+            files = [create_xml(contract_procedure(path))]
+        else:
+            print('Нет обработки для пакета:', type_xml)
+            sys.exit(1)
 
-    files_to_send.extend(files)
-    files_for_validation.extend(files)
+        files_to_send.extend(files)
+        files_for_validation.extend(files)
 
-    if validation:
-        for file in files_for_validation:
-            validate_xsd(file)
+        if validation:
+            for file in files_for_validation:
+                validate_xsd(file)
 
-    if send:
-        for file in files_to_send:
-            to_sent_to_sftp(file, server_address)
+        if send:
+            for file in files_to_send:
+                to_sent_to_sftp(file, server_address)
 
-    remove_file(path, 'archive')
-    test_folder(server_address)
+        remove_file(path, 'archive')
+        test_folder(server_address)
+    except OSError:
+        print('Ошибка! Необходимо указать правильный путь к файлу и заново запустить программу!')
 
 
 if __name__ == '__main__':
-    main('outgoing/14433643_xml.xml')
-
+    main('outgoing/14433571_xml.xml')
