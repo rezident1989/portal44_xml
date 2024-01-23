ce63d7249cce077cd144f0b26d784d87a05f7aee
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2024-01-23 14:36:10 +0300

    Обновление алгоритма обработки пакета tender_plan.py v.2

--- src/actions/tender_plan.py
+++ src/actions/tender_plan.py
@@ -13,7 +13,7 @@ def tender_plan_2020(outgoing_xml):
     main = template.find(".//ns4:tenderPlan2020", ns)
     number_position = 4
 
-    # Заполнение блока tenderPlan2020
+    # Заполнить простые теги блока tenderPlan2020
     main.attrib['schemeVersion'] = tree.find('.//ns1:data', ns).attrib.get('schemeVersion')
     template.find('.//ns3:id', ns).text = random_number(8)
     template.find('.//ns3:externalId', ns).text = tree.find('.//ns3:externalId', ns).text
@@ -25,46 +25,47 @@ def tender_plan_2020(outgoing_xml):
 
     template.find('.//ns3:versionNumber', ns).text = tree.find('.//ns3:versionNumber', ns).text
 
-    # Заполнение блока commonInfo
-    main.remove(template.find(".//ns3:commonInfo", ns))
-    main.insert(number_position, copy.deepcopy(tree.find(".//ns3:commonInfo", ns)))
+    # Заполнить блок commonInfo
+    path = './/ns3:commonInfo'
+    main.remove(template.find(path, ns))
+    main.insert(number_position, copy.deepcopy(tree.find(path, ns)))
 
     confirm_date = ET.Element('{http://zakupki.gov.ru/oos/TPtypes/1}confirmDate')
     confirm_date.text = tree.find('.//ns1:createDateTime', ns).text
-    template.find('.//ns3:commonInfo', ns).insert(2, confirm_date)
+    template.find(path, ns).insert(2, confirm_date)
 
     publish_date = ET.Element('{http://zakupki.gov.ru/oos/TPtypes/1}publishDate')
     publish_date.text = tree.find('.//ns1:createDateTime', ns).text
-    template.find('.//ns3:commonInfo', ns).insert(3, publish_date)
+    template.find(path, ns).insert(3, publish_date)
 
-    # Заполнение необязательного блока positions
+    # Заполнить необязательный блок positions
     try:
-        main.remove(template.find(".//ns3:positions", ns))
-        main.insert(number_position + 1, copy.deepcopy(tree.find(".//ns3:positions", ns)))
+        path = './/ns3:positions'
+        main.remove(template.find(path, ns))
+        main.insert(number_position + 1, copy.deepcopy(tree.find(path, ns)))
+        for position in template.findall(f'{path}/ns3:position/ns3:commonInfo', ns):
+            try:
+                position.find('.//ns3:positionNumber', ns).text
+            except AttributeError:
+                child = ET.Element('{http://zakupki.gov.ru/oos/TPtypes/1}positionNumber')
+                child.text = f'2024{random_number(20)}'
+                position.insert(0, child)
     except TypeError:
         number_position = number_position - 1
 
-    for position in template.findall('.//ns3:positions/ns3:position/ns3:commonInfo', ns):
-        try:
-            position.find('.//ns3:positionNumber', ns).text
-        except AttributeError:
-            child = ET.Element('{http://zakupki.gov.ru/oos/TPtypes/1}positionNumber')
-            child.text = f'2024{random_number(20)}'
-            position.insert(0, child)
-
-    # Заполнение необязательного блока specialPurchasePositions
+    # Заполнить необязательный блок specialPurchasePositions
     try:
-        main.remove(template.find(".//ns3:specialPurchasePositions", ns))
-        main.insert(number_position, copy.deepcopy(tree.find(".//ns3:specialPurchasePositions", ns)))
+        path = './/ns3:specialPurchasePositions'
+        main.remove(template.find(path, ns))
+        main.insert(number_position + 2, copy.deepcopy(tree.find(path, ns)))
+        for special_position in template.findall(f'{path}/ns3:specialPurchasePosition', ns):
+            try:
+                special_position.find('.//ns3:positionNumber', ns).text
+            except AttributeError:
+                child = ET.Element('{http://zakupki.gov.ru/oos/TPtypes/1}positionNumber')
+                child.text = f'2024{random_number(20)}'
+                special_position.insert(0, child)
     except TypeError:
         pass
-
-    for special_position in template.findall('.//ns3:specialPurchasePositions/ns3:specialPurchasePosition', ns):
-        try:
-            special_position.find('.//ns3:positionNumber', ns).text
-        except AttributeError:
-            child = ET.Element('{http://zakupki.gov.ru/oos/TPtypes/1}positionNumber')
-            child.text = f'2024{random_number(20)}'
-            special_position.insert(0, child)
 
     return template
