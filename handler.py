532d064742b702ddc28fa5e310fea9be9310a0ad
Aleksey Lazarev <lazarev.a@imc.spb.ru>
2024-09-16 14:43:16 +0300

    Крупное обновление 1

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ from actions.contract.contract import contract
 from actions.contract.contract_procedure import contract_procedure
 from actions.notification.ef_2020 import ef_notification, ef_submit_offers, ef_final_part_protocol
 from actions.tender_plan import tender_plan_2020
-from src.system_functions import clear_folder, get_type_xml, validate_xsd, get_path_xml
+from src.system_functions import clear_folder, get_type_xml, validate_xsd, get_path_xml, remove_file
 
 
 def handler():
@@ -18,10 +18,10 @@ def handler():
     if type_xml == 'tenderPlan2020Change':  # Тендер-план
         tender_plan_2020(path)
     elif type_xml == 'epNotificationEF2020':  # Электронный аукцион
-        confirmation(path, send=False)
-        data = ef_notification(path, send=False)
-        ef_submit_offers(data, send=False)
-        ef_final_part_protocol(data, send=False)
+        confirmation(path)
+        data = ef_notification(path)
+        ef_submit_offers(data)
+        ef_final_part_protocol(data)
     # elif type_xml == 'epNotificationEZT2020':  # "Закупка с полки" (или Закупка товаров согласно ч.12 ст. 93 № 44-ФЗ)
     #     confirmation(path)
     #     ezt_notification(path)
@@ -43,7 +43,7 @@ def handler():
         print('Нет обработки для пакета:', type_xml)
         sys.exit(1)
 
-    # remove_file(path, 'archive')
+    remove_file(path, 'archive')
     print('Программа закончила обработку пакетов')
 
 
